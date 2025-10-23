a9c0f1d1988925545ab2bb47ae219d5ee9a88a04
0b3hi <obehi.julius-imoisi@pau.edu.ng>
2025-10-23 12:45:30 +0100

    Project_2

---
+++ week-4/Project_2/src/main.rs
@@ -0,0 +1,38 @@
+//Rust program to determine the annual incentive of an employee.
+
+use std::io;
+
+fn main() {
+  
+  let mut experience = String::new();
+  let mut age = String::new();
+
+
+    println!("Are you experienced in this field? yes or no: ");
+    io::stdin().read_line(&mut experience).expect("Not a valid string");
+    let experience:&str = experience.trim();
+
+    println!("Enter your age: ");
+    io::stdin().read_line(&mut age).expect("Not a valid string");
+    let age:u32 = age.trim().parse().expect("Not a valid number");
+
+     if experience == "yes" {
+
+        if age >= 40{
+            println!("The annual incentive of the employee is 1560000 naira");
+    }
+        else if age >= 30 && age < 40{
+            println!("The annual incentive of the employee is 1480000 naira");
+    }
+        else{
+            println!("The monthly incentive of the employee is 1300000 naira");
+    }
+     
+    };
+
+    if experience == "no"{
+        println!("Your annual incentive is 100,000 naira")
+    }
+  }
+
+
